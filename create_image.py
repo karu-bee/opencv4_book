042e8ff6396dcd08e679b00e15391bc03c74272c
karu_bee <karubee.643@gmail.com>
2022-02-24 11:05:23 +0900

    1st commit

---
+++ create_image.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+img = np.zeros((400,400,3), np.uint8)
+img[:,:] = [255,0,0]
+cv2.imwrite('blueImage.jpg', img)
+cv2.imshow('img1',img)
+img[:,:] = [0,255,0]
+cv2.imwrite('greenImage.jpg', img)
+cv2.imshow('img2',img)
+img[:,:] = [0,0,255]
+cv2.imwrite('redImage.jpg', img)
+cv2.imshow('img3',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
